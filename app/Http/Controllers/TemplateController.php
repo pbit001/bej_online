244f77ad1da428af49b651dd4ef4422b6d311475
waqar.ahmed <waqar.ahmed@passu.tech>
2022-07-20 18:29:18 +0500

    pdf

--- app/Http/Controllers/TemplateController.php
+++ app/Http/Controllers/TemplateController.php
@@ -121,7 +121,7 @@ class TemplateController extends Controller
     public function download_template(Request $request)
     {
         $input = $request->all();
-        
+
         $templates = Template::where('id', '6')->first();
 
         $templateText = $templates->template_text;
@@ -142,7 +142,7 @@ class TemplateController extends Controller
 
             $templateToPDF .= $templateText;
         }
-
+        dd($templateToPDF);
 
         $data = [
             'title' => 'Welcome to ItSolutionStuff.com',
