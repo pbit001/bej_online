a5455d008767d1d5b631bb0cda9f5717c906e416
waqar.ahmed <waqar.ahmed@passu.tech>
2022-07-21 10:05:09 +0500

    pdf

--- routes/web.php
+++ routes/web.php
@@ -179,8 +179,8 @@ Route::delete('template/{template}', [TemplateController::class, 'destroy'])
     ->name('template.destroy')
     ->middleware('auth');
 
-Route::any('download_template_pdf', [TemplateController::class, 'download_template'])
-    ->name('download_template_pdf')
+Route::any('download_template', [TemplateController::class, 'download_template'])
+    ->name('download_templaten')
     ->middleware('auth');
     
 // Contacts
